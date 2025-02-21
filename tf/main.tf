24a40c312aac03bb6cbb51da141f719656d7f659
shibadog <shiba_dog@outlook.com>
2025-02-21 10:46:10 +0000

    added s3 bucket

--- tf/main.tf
+++ tf/main.tf
@@ -8,6 +8,27 @@ terraform {
       source  = "change-engine/slack-app"
       version = "~> 0.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+# S3バケットの定義
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-sample-slack-bolt"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
